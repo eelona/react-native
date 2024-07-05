4cdf83fc168ff7b7b52c56e14f42ead3a25edf2c
elonathaqi <eelonathaqi@gmail.com>
2024-07-05 17:42:14 +0200

    added style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,14 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -11,3 +16,25 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "lightpink",
+    borderRadius: 8,
+    elevation: 4,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "brown",
+    borderBottomWidth: 2,
+    color: "brown",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
