140b9655c05238fa7706a2d14e2c48b7e688be9b
elonathaqi <eelonathaqi@gmail.com>
2024-07-05 16:24:33 +0200

    game-project

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
